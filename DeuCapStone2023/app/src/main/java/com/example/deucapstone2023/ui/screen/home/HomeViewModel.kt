3145133da335c28e04264695e1e1326c42517f19
jino <whangwan@gmail.com>
2023-04-30 23:32:03 +0900

    feat : 하단바 추가 및 mainActivity 수정
     - TTS (Text To Speak) 추가
     - STT (Speak To Text) 추가
     - 하단바 및 아이콘 추가

--- DeuCapStone2023/app/src/main/java/com/example/deucapstone2023/ui/screen/home/HomeViewModel.kt
+++ DeuCapStone2023/app/src/main/java/com/example/deucapstone2023/ui/screen/home/HomeViewModel.kt
@@ -1,9 +1,30 @@
 package com.example.deucapstone2023.ui.screen.home
 
 import androidx.lifecycle.ViewModel
+import com.skt.tmap.TMapView
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
+data class HomeUiState(
+    val title: String
+) {
+    companion object {
+        fun getInitValues() = HomeUiState(
+            title = ""
+        )
+    }
+}
+
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+) : ViewModel() {
+    private val _homeUiState = MutableStateFlow(HomeUiState.getInitValues())
+    val homeUiState get() = _homeUiState.asStateFlow()
+
+    fun setTitle(title: String) = _homeUiState.update { state ->
+        state.copy(title = title)
+    }
 }
