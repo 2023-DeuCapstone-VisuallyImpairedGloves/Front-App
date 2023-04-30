83b3649b66b3adb4620f7abd7a4e51229c5bff18
jino <whangwan@gmail.com>
2023-04-30 23:37:58 +0900

    feat : 공통 컴포넌트 추가

--- DeuCapStone2023/app/src/main/java/com/example/deucapstone2023/ui/component/Layout.kt
+++ DeuCapStone2023/app/src/main/java/com/example/deucapstone2023/ui/component/Layout.kt
@@ -2,14 +2,37 @@ package com.example.deucapstone2023.ui.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.deucapstone2023.ui.theme.white
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DefaultLayout(
+    modifier: Modifier = Modifier,
+    topBar: @Composable () -> Unit = {},
+    bottomBar: @Composable () -> Unit = {},
+    content: @Composable (PaddingValues) -> Unit
+) {
+    androidx.compose.material3.Scaffold(
+        topBar = { topBar() },
+        bottomBar = { bottomBar() },
+        modifier = modifier,
+        containerColor = white
+    ) {
+        content(it)
+    }
+
+}
+
+@Composable
+fun SnackBarLayout(
+    modifier: Modifier = Modifier,
     topBar: @Composable () -> Unit = {},
     scaffoldState: ScaffoldState,
     content: @Composable (PaddingValues) -> Unit
@@ -19,14 +42,16 @@ fun DefaultLayout(
         scaffoldState = scaffoldState,
         snackbarHost = { snackBarHostState ->
             Column() {
-                SnackBarHostCustom(headerMessage = snackBarHostState.currentSnackbarData?.message ?: "",
+                SnackBarHostCustom(headerMessage = snackBarHostState.currentSnackbarData?.message
+                    ?: "",
                     contentMessage = snackBarHostState.currentSnackbarData?.actionLabel ?: "",
                     snackBarHostState = scaffoldState.snackbarHostState,
                     disMissSnackBar = { scaffoldState.snackbarHostState.currentSnackbarData?.dismiss() })
                 VerticalSpacer(height = 90.dp)
             }
         },
-        backgroundColor = white
+        backgroundColor = white,
+        modifier = modifier.fillMaxSize()
     ) {
         content(it)
     }
