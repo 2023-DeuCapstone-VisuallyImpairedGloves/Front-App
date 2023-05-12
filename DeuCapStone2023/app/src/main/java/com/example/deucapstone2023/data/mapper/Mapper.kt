5fb12fe321fb2ebd13f002d214e078f17a300fac
jino <whangwan@gmail.com>
2023-05-12 19:12:12 +0900

    Revert "Merge branch 'main' of https://github.com/2023-DeuCapstone-VisuallyImpairedGloves/Front-App into feat/search"
    This reverts commit e1b05f3d568989af922b85a82efbf153a9915ed3.

--- DeuCapStone2023/app/src/main/java/com/example/deucapstone2023/data/mapper/Mapper.kt
+++ DeuCapStone2023/app/src/main/java/com/example/deucapstone2023/data/mapper/Mapper.kt
@@ -36,3 +36,139 @@ fun Poi.toPOIModel() = POIModel(
     centerLon = noorLon?.toDouble() ?: .0,
     searchtypCd = ""
 )
+
+fun RouteResponse.toRouteModel() = this.run {
+    val list = mutableListOf<RouteModel>()
+
+    var routeName = ""
+    var routeDescription = ""
+    var routeTurnType = 0
+    var routePointType = ""
+    var routeFacilityType = 0
+    var startLatitude = 0.0
+    var startLongitude = 0.0
+    var destinationLatitude = 0.0
+    var destinationLongitude = 0.0
+    var totalDistance = 0
+    var totalTime = 0
+    val lineInfo = mutableListOf<LineModel>()
+
+    this.features?.forEachIndexed { index, feature ->
+        when (feature.geometry?.type) {
+            "Point" -> {
+                if (index != 0)
+                    list.add(
+                        RouteModel(
+                            name = routeName,
+                            description = routeDescription,
+                            turnType = routeTurnType,
+                            pointType = routePointType,
+                            facilityType = routeFacilityType,
+                            startLatitude = startLatitude,
+                            startLongitude = startLongitude,
+                            destinationLatitude = destinationLatitude,
+                            destinationLongitude = destinationLongitude,
+                            lineInfo = lineInfo.distinct(),
+                            totalDistance = totalDistance,
+                            totalTime = totalTime
+                        )
+                    )
+
+                val location = feature.geometry.coordinates
+
+                startLatitude = location?.get(1) as Double
+                startLongitude = location.get(0) as Double
+                routeDescription = feature.properties?.description ?: ""
+                routeTurnType = feature.properties?.turnType ?: 1
+                routePointType = feature.properties?.pointType ?: ""
+                routeName = ""
+                routeFacilityType = 0
+                destinationLatitude = 0.0
+                destinationLongitude = 0.0
+                totalDistance = 0
+                totalTime = 0
+                lineInfo.clear()
+
+                if(index == features.lastIndex)
+                    list.add(
+                        RouteModel(
+                            name = routeName,
+                            description = routeDescription,
+                            turnType = routeTurnType,
+                            pointType = routePointType,
+                            facilityType = routeFacilityType,
+                            startLatitude = startLatitude,
+                            startLongitude = startLongitude,
+                            destinationLatitude = destinationLatitude,
+                            destinationLongitude = destinationLongitude,
+                            lineInfo = lineInfo,
+                            totalDistance = totalDistance,
+                            totalTime = totalTime
+                        )
+                    )
+            }
+
+            "LineString" -> {
+                val location = feature.geometry.coordinates
+                destinationLatitude = (((location?.last() ?: .0) as List<*>?)?.get(1) ?: .0) as Double
+                destinationLongitude = (((location?.last() ?: .0) as List<*>?)?.get(0) ?: .0) as Double
+                routeName = feature.properties?.name ?: ""
+                routeFacilityType = if (feature.properties?.facilityType?.isNotBlank() == true) feature.properties.facilityType.toInt() ?: 0 else 0
+                totalDistance = feature.properties?.distance ?: 0
+                totalTime = feature.properties?.time ?: 0
+                lineInfo.addAll(location?.map {
+                    val eachLocation = (it as List<*>)
+                    LineModel(eachLocation[1] as Double, eachLocation[0] as Double)
+                } ?: emptyList())
+            }
+        }
+    }
+
+    list.toList()
+}
+
+
+/*this.features?.forEachIndexed { index,feature ->
+when (features.lastIndex == 0) {
+    true -> {
+        val location = features[0].geometry?.coordinates
+
+        RouteModel(
+            startLatitude = location?.get(0) as Double,
+            startLongitude = location.get(1) as Double,
+            destinationLatitude = .0,
+            destinationLongitude = .0,
+            name = "",
+            description = features[0].properties?.description ?: "",
+            turnType = features[0].properties?.turnType ?: 1,
+            pointType = features[0].properties?.pointType ?: "",
+            facilityType = 0,
+            totalDistance = 0,
+            totalTime = 0,
+            lineInfo = emptyList()
+        )
+    }
+
+    false -> {
+        val location = features[1].geometry?.coordinates
+
+        RouteModel(
+            startLatitude = (((location?.first() ?: .0) as List<*>?)?.get(0) ?: .0) as Double,
+            startLongitude = (((location?.first() ?: .0) as List<*>?)?.get(1) ?: .0) as Double,
+            destinationLatitude = (((location?.last() ?: .0) as List<*>?)?.get(0) ?: .0) as Double,
+            destinationLongitude = (((location?.last() ?: .0) as List<*>?)?.get(1) ?: .0) as Double,
+            name = features[1].properties?.name ?: "",
+            description = features[0].properties?.description ?: "",
+            turnType = features[0].properties?.turnType ?: 0,
+            pointType = features[0].properties?.pointType ?: "",
+            facilityType = if (features[1].properties?.facilityType?.isNotBlank() == true) features[1].properties?.facilityType?.toInt() ?: 0 else 0,
+            totalDistance = features[1].properties?.distance ?: 0,
+            totalTime = features[1].properties?.time ?: 0,
+            lineInfo = location?.map {
+                val eachLocation = (it as List<*>)
+                LineModel(eachLocation[1] as Double, eachLocation[0] as Double)
+            } ?: emptyList()
+        )
+    }
+}
+} ?: emptyList()*/
--- DeuCapStone2023/app/src/main/java/com/example/deucapstone2023/ui/navigation/MainActivity.kt
+++ DeuCapStone2023/app/src/main/java/com/example/deucapstone2023/ui/navigation/MainActivity.kt
@@ -25,7 +25,9 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
 import com.example.deucapstone2023.R
-import com.example.deucapstone2023.ui.screen.home.search.SearchViewModel
+import com.example.deucapstone2023.ui.base.CommonRecognitionListener
+import com.example.deucapstone2023.ui.screen.search.SearchViewModel
+import com.example.deucapstone2023.ui.service.SpeechService
 import com.example.deucapstone2023.ui.theme.DeuCapStone2023Theme
 import com.skt.tmap.TMapGpsManager
 import com.skt.tmap.TMapPoint
@@ -154,18 +156,22 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private fun requireLaunchingPermission() {
+        locationPermissionRequest.launch(PERMISSIONS)
+    }
+
     private fun initState() {
         tMapGpsManager = TMapGpsManager(this).apply {
-            minTime = 1000
-            minDistance = 5F
-            provider = TMapGpsManager.PROVIDER_NETWORK
+            minTime = 8000
+            minDistance = 4.5F
+            provider = TMapGpsManager.PROVIDER_GPS
             setOnLocationChangeListener { location ->
                 searchViewModel::setUserLocation.invoke(location.latitude, location.longitude)
                 tMapView.apply {
                     setCenterPoint(location.latitude, location.longitude)
-                    zoomLevel = 15
-                    if (getMarkerItem2FromID("UserPosition") != null)
-                        removeTMapMarkerItem2("UserPosition")
+                    zoomLevel = 18
+                    if (getMarkerItemFromId("UserPosition") != null)
+                        removeTMapMarkerItem("UserPosition")
                     addTMapMarkerItem(TMapMarkerItem().apply {
                         tMapPoint = TMapPoint(location.latitude, location.longitude)
                         icon = BitmapFactory.decodeResource(
@@ -177,59 +183,10 @@ class MainActivity : ComponentActivity() {
                     })
                 }
             }
-            openGps()
         }
 
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this).apply {
-            setRecognitionListener(object : RecognitionListener {
-                override fun onReadyForSpeech(params: Bundle?) {
-
-                }
-
-                override fun onBeginningOfSpeech() {
-                    Log.d("test", "시작")
-                }
-
-                override fun onRmsChanged(rmsdB: Float) {
-
-                }
-
-                override fun onBufferReceived(buffer: ByteArray?) {
-
-                }
-
-                override fun onEndOfSpeech() {
-
-                }
-
-                override fun onError(error: Int) {
-                    voiceOutput("다시 말씀해 주실래요?")
-                }
-
-                override fun onResults(results: Bundle?) {
-                    val userSpeech =
-                        results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
-                    searchViewModel.searchPlace(
-                        this@MainActivity.getString(R.string.T_Map_key),
-                        userSpeech?.get(0) ?: "당감댁"
-                    )
-                    if (userSpeech?.get(0) == null) {
-                        voiceOutput("다시 말씀해 주실래요?")
-                    } else {
-                        voiceOutput("사용자 입력 메세지는 ${userSpeech[0] ?: ""} 입니다.")
-                    }
-                }
-
-                override fun onPartialResults(partialResults: Bundle?) {
-
-                }
-
-                override fun onEvent(eventType: Int, params: Bundle?) {
-
-                }
-
-            })
-        }
+        tMapGpsManager.openGps()
+        startService(Intent(this, SpeechService::class.java))
 
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
 
